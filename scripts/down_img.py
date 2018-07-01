19fce3b46961174ad65752d755f6b63c6397f699
edward zhu <hellohyesun@outlook.com>
2018-07-01 20:32:41 +0800

    use openpyxl instead for large file i/o.

--- scripts/down_img.py
+++ scripts/down_img.py
@@ -1,21 +1,22 @@
 import math
 import os
-import requests
 import threading
 
-import xlrd
-import xlwt
+import openpyxl
+import requests
 
 PROJ_ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 FILE_DIR = os.path.join(PROJ_ROOT_DIR, 'files')
 IMG_DIR = os.path.join(FILE_DIR, 'images')
-TOUTIAO_ANNO_FILE_PATH = os.path.join(FILE_DIR, 'toutiao_content_image.xls')
-OUTPUT_XLS_FILE_PATH = os.path.join(FILE_DIR, 'article_attr.xls')
+TOUTIAO_ANNO_FILE_PATH = os.path.join(FILE_DIR, 'toutiao_content_image.xlsx')
+OUTPUT_XLSX_FILE_PATH = os.path.join(FILE_DIR, 'article_attr.xlsx')
 TOUTIAO_ANNO_TITLES = ('article_title', 'article_class', 'tags', 'article_url',
                        'img_urls', 'article_content')
-OUTPUT_XLS_TITLES = ('article_id', 'img_urls', 'img_paths', 'has_content',
-                     'contain_car', 'contain_human', 'human_tag',
-                     'object tags')
+OUTPUT_XLSX_TITLES = ('article_id', 'img_urls', 'img_paths', 'has_content',
+                      'contain_car', 'contain_human', 'human_tag',
+                      'object tags')
+MAX_IMG_PER_ARTICLE = 5
+NUM_THREADS = 40
 
 
 def down_img(url, save_path):
@@ -31,23 +32,27 @@ def down_img(url, save_path):
         return False
 
 
-def process_article(output_xls_sheet, article_id, row_value):
+def process_article(article_id, input_xlsx_sheet, output_xlsx_sheet):
     """Performs image downloading and data processing for an article.
 
-    :param output_xls_sheet: The xls sheet to write (xlwt.Workbook.Sheet).
     :param article_id: Index for the article in the anno file (int).
-    :param row_value: The data for that article in the anno file (list).
-    :returns: None
-    :rtype: None
-
+    :param input_xlsx_sheet: The sheet to read (openpyxl worksheet).
+    :param output_xlsx_sheet: The sheet to write (openpyxl worksheet).
     """
-    article_class = row_value[1].strip()
-    img_urls = row_value[4].strip()
-    article_content = row_value[-1].strip()
+    # row and column index start from 1
+    row_index = article_id + 1
+    article_class = input_xlsx_sheet.cell(
+        row=row_index, column=2).value.strip()
+    img_urls = input_xlsx_sheet.cell(row=row_index, column=5).value
+    article_content = input_xlsx_sheet.cell(row=row_index, column=6).value
 
-    img_url_list = img_urls.split(',') if img_urls else []
+    if not img_urls:
+        img_url_list = []
+    else:
+        num_imgs = min(len(img_urls), MAX_IMG_PER_ARTICLE)
+        img_url_list = img_urls.strip().split(',')[:num_imgs]
     img_path_list = []
-    has_content = (article_content != '')
+    has_content = (article_content is not None)
     contain_car = '汽车' in article_class
     article_dir = os.path.join(IMG_DIR, 'article_' + str(article_id))
     non_prefix_article_dir = os.path.join(
@@ -70,52 +75,50 @@ def process_article(output_xls_sheet, article_id, row_value):
 
     img_paths = ','.join(img_path_list)
 
-    # article_id + 1 because the first row of the sheet is the tile row
-    output_xls_sheet.write(article_id + 1, 0, article_id)
-    output_xls_sheet.write(article_id + 1, 1, img_urls)
-    output_xls_sheet.write(article_id + 1, 2, img_paths)
-    output_xls_sheet.write(article_id + 1, 3, int(has_content))
-    output_xls_sheet.write(article_id + 1, 4, int(contain_car))
-    output_xls_sheet.write(article_id + 1, 5, '')
-    output_xls_sheet.write(article_id + 1, 6, '')
-    output_xls_sheet.write(article_id + 1, 7, '')
+    # row_index + 1 because the first row of the sheet is the title row
+    output_xlsx_sheet.cell(row=row_index + 1, column=1).value = article_id
+    output_xlsx_sheet.cell(row=row_index + 1, column=2).value = img_urls
+    output_xlsx_sheet.cell(row=row_index + 1, column=3).value = img_paths
+    output_xlsx_sheet.cell(
+        row=row_index + 1, column=4).value = int(has_content)
+    output_xlsx_sheet.cell(
+        row=row_index + 1, column=5).value = int(contain_car)
+    output_xlsx_sheet.cell(row=row_index + 1, column=6).value = ''
+    output_xlsx_sheet.cell(row=row_index + 1, column=7).value = ''
+    output_xlsx_sheet.cell(row=row_index + 1, column=8).value = ''
 
 
-def process_split(split, input_xls_sheet, output_xls_sheet):
+def process_split(split, input_xlsx_sheet, output_xlsx_sheet):
     """Processes a list of articles in the split.
 
     :param split: The indices of the articles (list).
-    :param input_xls_sheet: The xls sheet to read from.
-    :param output_xls_sheet: The xls sheet to write.
-    :returns: None.
-    :rtype: None.
-
+    :param input_xlsx_sheet: The xlsx sheet to read (openpyxl worksheet).
+    :param output_xlsx_sheet: The xlsx sheet to write (openpyxl worksheet).
     """
     for article_id in split:
-        row_value = input_xls_sheet.row_values(article_id)
-        process_article(output_xls_sheet, article_id, row_value)
+        process_article(article_id, input_xlsx_sheet, output_xlsx_sheet)
 
 
 def main():
     # prepare the input and output excel files
-    toutiao_anno_file = xlrd.open_workbook(TOUTIAO_ANNO_FILE_PATH)
-    toutiao_anno_sheet = toutiao_anno_file.sheet_by_index(0)
-    output_xls_file = xlwt.Workbook()
-    output_xls_sheet = output_xls_file.add_sheet(
-        'toutiao', cell_overwrite_ok=True)
+    toutiao_anno_workbook = openpyxl.load_workbook(TOUTIAO_ANNO_FILE_PATH)
+    toutiao_anno_sheet = toutiao_anno_workbook.active
+    output_xlsx_workbook = openpyxl.Workbook()
+    output_xlsx_sheet = output_xlsx_workbook.active
 
-    for i, title in enumerate(OUTPUT_XLS_TITLES):
-        output_xls_sheet.write(0, i, title)
+    for i, title in enumerate(OUTPUT_XLSX_TITLES):
+        # row and column index starts from 1
+        output_xlsx_sheet.cell(row=1, column=i + 1).value = title
 
     # TODO: remove '[:100]' to access the full list
-    article_ids = list(range(0, toutiao_anno_sheet.nrows))[:100]
-    n_splits = 8
-    split_size = math.ceil(len(article_ids) / n_splits)
+    article_ids = list(range(0, toutiao_anno_sheet.max_row))[:80000]
+    n_threads = NUM_THREADS
+    split_size = math.ceil(len(article_ids) / n_threads)
     splits = []
 
-    for i_split in range(n_splits):
-        start = i_split * split_size
-        end = (i_split + 1) * split_size
+    for i_thread in range(n_threads):
+        start = i_thread * split_size
+        end = (i_thread + 1) * split_size
         if end > len(article_ids):
             end = len(article_ids)
         split = article_ids[start:end]
@@ -125,14 +128,14 @@ def main():
     for split in splits:
         thread = threading.Thread(
             target=process_split,
-            args=(split, toutiao_anno_sheet, output_xls_sheet))
+            args=(split, toutiao_anno_sheet, output_xlsx_sheet))
         thread.start()
         threads.append(thread)
 
     for thread in threads:
         thread.join()
 
-    output_xls_file.save(OUTPUT_XLS_FILE_PATH)
+    output_xlsx_workbook.save(OUTPUT_XLSX_FILE_PATH)
 
 
 if __name__ == '__main__':
